8a2c3aa761555697e674372baf76decfa7c3ba4e
EloizaBAB <122025679+EloizaBAB@users.noreply.github.com>
2023-02-08 16:55:29 +0000

    API integration
    in the function displayForecast created a variable to store the array with the days,

--- src/script.js
+++ src/script.js
@@ -32,6 +32,12 @@ function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
+function formatDay(timestamp) {
+  let date = new date(timestamp * 1000);
+  let forecastDay = date.getDay();
+  let forecastDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return forecastDay;
+}
 function getForecast(coordinates) {
   let apiKey = "8161b4309ee03faae957729ba7104797";
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
@@ -61,23 +67,33 @@ function showTemperature(response) {
 }
 
 function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["wed", "thurs", "fri", "sat"];
-  days.forEach(function (day) {
+
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `
               <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
                 <img
-                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt=""
                   width="20px"
                 />
                 <div class="weather-forecast-temperatures">
-                  <span class="weater-forecast-maxtemp">18</span>
-                  <span class="weather-forecast-mintemp">12</span>
+                  <span class="weater-forecast-maxtemp">${
+                    forecastDay.temp.max
+                  }</span>
+                  <span class="weather-forecast-mintemp">${
+                    forecastDay.temp.min
+                  }</span>
                 </div>
               </div>`;
   });
