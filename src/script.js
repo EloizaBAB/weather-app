30043ec43560c9b20cdec4387cebd18d096144d0
EloizaBAB <122025679+EloizaBAB@users.noreply.github.com>
2023-03-03 21:34:05 +0000

    Added documentation to the code

--- src/script.js
+++ src/script.js
@@ -1,3 +1,12 @@
+/**
+ *
+ * @param {integer} a number 1-7 that corresponds to each day of the week
+ * @returns the current date and time (local timezone)
+ * Created an array with the name of the days of the week in one variable and used the method .getDay() that gives a number between 1 and 7 as index and  get the day of the week
+ * To get the hours I created a variable and used the method .now.getHours() that returns the hour for the specified date,according to local time
+ * Created a variable to store the hours and minutes and used the methods now.getHours() and .now.getMinutes(),To format the time as I want (00:00) I had to create a conditions (variable <10), and use template literals to display it correctly
+ * To display the current date and time I created a variable (currentDate) and  used template literals(``) to embed an expression into a part of a string,copied and pasted the block of code and replaced some parts with the  placeholders of the form ${expression} to perform substitutions for embedded expressions
+ */
 function formatDate(date) {
   let days = [
     "Sunday",
@@ -20,14 +29,35 @@ function formatDate(date) {
   let currentDate = `${day},Your local time : ${time}:${minutes}`;
   return currentDate;
 }
+
+/**
+ * API integration:
+ * I used the https://openweathermap.org/ to get a free API with weather data.
+ *Then, I created variable to store my apiKey that I got from the OpenWeather and another one for the apiUrl ,which I used the template literals with the expressions to get the coordenates. I looked through the api documentation to find out how to specify the coordenates,the apiKey and the units I wanted.
+ *selected the element where I wanted the date to appear and used the method new Date() to get the date and format it.
+ */
 let now = new Date();
 let date = document.querySelector("#date");
 date.innerHTML = formatDate();
+
+/**
+ *
+ * @param {number} position based on the latitude and longitude.
+ * Used Axios, it has the ability to make HTTP requests from the browser and handle the transformation of request and response data.
+ * I used the AJAX technique to this web application be able to make quick, incremental updates to the user interface without reloading the entire browser page. This makes the application faster and more responsive to user actions.
+ *Used the method get().then() to be abble to display the temperature based on the coordenates.
+ */
 function searchLocation(position) {
   let apikey = "8161b4309ee03faae957729ba7104797";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apikey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 }
+
+/**
+ *
+ * @param {click} event a click form the button "Your current location".
+ * Used the method preventDefault (click) that cancels the event if it is cancelable,so the default action that belongs to the event will not occur.
+ */
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
